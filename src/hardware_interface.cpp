75004a49fa2ba10d089aeb8037a6eebf10910d64
Jose Fajardo <jmfajardod@unal.edu.co>
2021-01-08 22:07:06 -0500

    Added soft limits in hardware interface

--- src/hardware_interface.cpp
+++ src/hardware_interface.cpp
@@ -58,6 +58,7 @@ void RobotHardwareInterface::init(){
     // Load URDF model
     urdf::Model urdf_model;
     if (!urdf_model.initParam("robot_description")) return;
+    ROS_INFO("Model loaded");
 
     // Declare number of joints
     num_joints_=6;
@@ -78,20 +79,38 @@ void RobotHardwareInterface::init(){
 
         // Create joint state interface
         hardware_interface::JointStateHandle jointStateHandle(joint_names_[i], &joint_position_[i], &joint_velocity_[i], &joint_effort_[i]);        
+        joint_state_interface_.registerHandle(jointStateHandle);
 
         // Create position joint interface
         hardware_interface::JointHandle jointPositionHandle(jointStateHandle, &joint_position_command_[i]);
+        position_joint_interface_.registerHandle(jointPositionHandle);
+    }
 
+    registerInterface(&joint_state_interface_);
+    registerInterface(&position_joint_interface_);
+
+    // Limits
+    for (int i = 0; i < num_joints_; ++i) {
         //--- Create limits
         joint_limits_interface::JointLimits limits;
         joint_limits_interface::SoftJointLimits soft_limits;
-
+        
         //--- Populate (soft) joint limits from URDF
         //--- Limits specified in URDF overwrite existing values in 'limits' and 'soft_limits'
         //--- Limits not specified in URDF preserve their existing values
         urdf::JointConstSharedPtr urdf_joint = urdf_model.getJoint(joint_names_[i]);
-        bool urdf_limits_ok = getJointLimits(urdf_joint, limits);
-        bool urdf_soft_limits_ok = getSoftJointLimits(urdf_joint, soft_limits);
+        getJointLimits(urdf_joint, limits);
+        getSoftJointLimits(urdf_joint, soft_limits);
+
+        // Define manually some limits
+        limits.has_acceleration_limits = false;
+        limits.has_effort_limits = false;
+        limits.has_jerk_limits = false;
+
+        soft_limits.max_position =  limits.max_position;
+        soft_limits.min_position =  limits.min_position;
+        soft_limits.k_position = 10.0;
+        soft_limits.k_velocity = 10.0;
 
         //--- Populate (soft) joint limits from the ros parameter server
         //--- Limits specified in the parameter server overwrite existing values in 'limits' and 'soft_limits'
@@ -99,17 +118,14 @@ void RobotHardwareInterface::init(){
         //const bool rosparam_limits_ok = getJointLimits(joint_names_[i], nh_, limits);
 
         //--- Create handle in joint limits interface
+        hardware_interface::JointHandle jointPositionHandle = position_joint_interface_.getHandle(joint_names_[i]);
         joint_limits_interface::PositionJointSoftLimitsHandle jointLimitsHandle(jointPositionHandle, limits, soft_limits);  // Soft limits spec
         
         //--- Register handles
-        joint_state_interface_.registerHandle(jointStateHandle);
         positionJointSoftLimitsInterface.registerHandle(jointLimitsHandle);
-        position_joint_interface_.registerHandle(jointPositionHandle);
     }
+    //registerInterface(&positionJointSoftLimitsInterface);
 
-    registerInterface(&joint_state_interface_);
-    registerInterface(&position_joint_interface_);
-    registerInterface(&positionJointSoftLimitsInterface);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -138,7 +154,9 @@ void RobotHardwareInterface::read(){
 // Write function
 void RobotHardwareInterface::write(ros::Duration elapsed_time){
 
-    //positionJointSoftLimitsInterface.enforceLimits(elapsed_time);
+    //ROS_INFO("Before enforce: %f",joint_position_command_[0] );
+    positionJointSoftLimitsInterface.enforceLimits(elapsed_time);
+    //ROS_INFO("After enforce: %f",joint_position_command_[0] );
     
     jt_cmds.data.clear();
     jt_cmds.data.push_back((joint_position_command_[0]-(-M_PI_2))*(180.0-0.0)/(M_PI_2-(-M_PI_2)) + (0.0));
